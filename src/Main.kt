0948b96a9c21dcb5206fd702e210a6a98279b56e
renatofelipemartinssilva <166918841+renatofelipemartinssilva@users.noreply.github.com>
2025-03-24 10:23:18 -0300

    a

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,58 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    val numero = 10
+    if (numero > 0) {
+        println("O número é positivo")
+    } else {
+        println("O número é negativo ou zero")
+    }
+
+    val opcao = 2
+    when (opcao) {
+        1 -> println("Opção 1 selecionada")
+        2 -> println("Opção 2 selecionada")
+        else -> println("Opção inválida")
+    }
 
     for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+        println("Contagem: $i")
     }
+
+    val array = arrayOf(10, 20, 30, 40, 50, 60, 70, 80)
+    println("Todos os itens do array:")
+    for (item in array) {
+        println(item)
+    }
+
+    if (array.size >= 6) {
+        println("O sexto elemento é: ${array[5]}")
+    } else {
+        println("O array não possui 6 elementos")
+    }
+
+    var contador = 0
+    while (contador < 5) {
+        println("Numero: $contador")
+        contador++
+    }
+
+    println("Soma: ${soma(10, 5)}")
+    println("Subtração: ${subtracao(10, 5)}")
+    println("Multiplicação: ${multiplicacao(10, 5)}")
+    println("Divisão: ${divisao(10, 5)}")
+}
+
+fun soma(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun subtracao(a: Int, b: Int): Int {
+    return a - b
+}
+
+fun multiplicacao(a: Int, b: Int): Int {
+    return a * b
+}
+
+fun divisao(a: Int, b: Int): Int {
+    return if (b != 0) a / b else 0
 }
